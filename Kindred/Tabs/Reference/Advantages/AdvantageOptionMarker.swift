9738b5afa59f2b9984e361144ddf6a1667cc7dbf
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-06-22 15:34:14 -0700

    Add fields for eventual work

---
+++ Kindred/Tabs/Reference/Advantages/AdvantageOptionMarker.swift
@@ -0,0 +1,20 @@
+//
+//  AdvantageOptionMarker.swift
+//  Kindred
+//
+//  Created by Jared Lindsay on 6/22/21.
+//
+
+import SwiftUI
+
+struct AdvantageOptionMarker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdvantageOptionMarker_Previews: PreviewProvider {
+    static var previews: some View {
+        AdvantageOptionMarker()
+    }
+}
