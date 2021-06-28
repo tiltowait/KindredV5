64633b0c0eb2e00d1ee55bce6d05c2613f4e6cdf
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-06-28 16:04:28 -0700

    Improve spacing algorithm

--- Kindred/Generic Views/DotSelector.swift
+++ Kindred/Generic Views/DotSelector.swift
@@ -33,18 +33,15 @@ struct DotSelector: View {
   var body: some View {
     HStack {
       ForEach(Int(min)...Int(max)) { index in
-        Circle()
-          .fill(color(for: index))
-          .frame(width: size, height: size)
+        // Make a gap every 5 dots for legibility purposes
+        if index > 0 && (index - 1) % 5 == 0 {
+          Color.clear.frame(width: 1, height: 1)
+        }
+        
+        shape(for: index)
           .onTapGesture {
             select(dots: index)
           }
-        
-        // Make a gap every 5 dots for legibility purposes
-        if index > 0 && index % 5 == 0 {
-          Circle().fill(Color.clear)
-            .frame(width: 1, height: 1)
-        }
       }
     }
   }
@@ -57,6 +54,23 @@ struct DotSelector: View {
   /// - Returns: The color the dot should take.
   func color(for rating: Int) -> Color {
     rating <= current ? .vampireRed : .tertiarySystemGroupedBackground
+  }
+  
+  /// Create a square for negative indices and a circle for positive.
+  ///
+  /// The shape is automatically sized and filled with the appropriate color.
+  /// - Parameter index: The shape's index.
+  /// - Returns: The generated shape.
+  @ViewBuilder func shape(for index: Int) -> some View {
+    if index < 0 {
+      Rectangle()
+        .fill(color(for: index))
+        .frame(width: size, height: size)
+    } else {
+      Circle()
+        .fill(color(for: index))
+        .frame(width: size, height: size)
+    }
   }
   
   /// Select the given number of dots. If "one" is double-tapped, set to 0.
@@ -73,6 +87,6 @@ struct DotSelector: View {
 
 struct DotSelector_Previews: PreviewProvider {
   static var previews: some View {
-    DotSelector(current: .constant(3), min: 1, max: 5)
+    DotSelector(current: .constant(3), min: 1, max: 11)
   }
 }
