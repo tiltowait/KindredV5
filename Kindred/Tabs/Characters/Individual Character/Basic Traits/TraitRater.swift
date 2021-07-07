fee80739081467feea9657f0fdfb5a83fa63c2b5
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-07-07 08:56:54 -0700

    Use DotSelector instead of custom jank

--- Kindred/Tabs/Characters/Individual Character/Basic Traits/TraitRater.swift
+++ Kindred/Tabs/Characters/Individual Character/Basic Traits/TraitRater.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 struct TraitRater: View {
   
-  @State private var showingReferenceAlert = false
-  let size: CGFloat = 17
-  let spacing: CGFloat = 5
-  
   @ObservedObject var kindred: Kindred
   let keyPath: ReferenceWritableKeyPath<Kindred, Int16>
-  let max: Int
+  let max: Int16
   let reference: String
+  
+  @State private var binding: Int16
+  @State private var showingReferenceAlert = false
+  
+  let size: CGFloat = 17
+  let spacing: CGFloat = 5
+
+  init(
+    kindred: Kindred,
+    keyPath: ReferenceWritableKeyPath<Kindred, Int16>,
+    max: Int16, reference: String
+  ) {
+    self.kindred = kindred
+    self.keyPath = keyPath
+    self.reference = reference
+    self.max = max
+    
+    _binding = State(wrappedValue: kindred[keyPath: keyPath])
+  }
   
   var label: String {
     keyPath.stringValue.unCamelCased.capitalized
@@ -29,20 +44,7 @@ struct TraitRater: View {
       
       Spacer()
       
-      ForEach(1...max) { index in
-        Circle()
-          .fill(color(for: index))
-          .frame(width: size, height: size)
-          .onTapGesture {
-            updateValue(to: index)
-          }
-        
-        // Make a gap every 5 dots for legibility purposes
-        if index > 0 && index % 5 == 0 {
-          Circle().fill(Color.clear)
-            .frame(width: 1, height: 1)
-        }
-      }
+      DotSelector(current: $binding, min: 1, max: max, allowZero: true)
       
       // Reference button
       Button {
@@ -55,28 +57,13 @@ struct TraitRater: View {
     .alert(isPresented: $showingReferenceAlert) {
       Alert(title: Text(label), message: Text(reference), dismissButton: .default(Text("OK")))
     }
+    .onChange(of: binding, perform: updateTrait)
   }
   
-  /// Determine the color for a dot at a given rating.
-  ///
-  /// If `rating` is greater than the receiver's current value, then the dot is an "unfilled" color.
-  /// Otherwise, it will receive a "filled" color.
-  /// - Parameter rating: The index of the dot in question.
-  /// - Returns: The color the dot should take.
-  func color(for rating: Int) -> Color {
-    rating <= kindred[keyPath: keyPath] ? .vampireRed : .tertiarySystemGroupedBackground
-  }
-  
-  /// Update the referenced value.
-  /// - Parameter newValue: The receiver's new value.
-  func updateValue(to newValue: Int) {
-    if newValue > 1 {
-      kindred[keyPath: keyPath] = Int16(newValue)
-    } else {
-      // If the current value is 1, set to 0. Otherwise, set to 1.
-      let current = kindred[keyPath: keyPath]
-      kindred[keyPath: keyPath] = current == 1 ? 0 : 1
-    }
+  /// Update the Kindred's trait (represented by the key path) to a new value.
+  /// - Parameter newValue: The trait's new value.
+  func updateTrait(_ newValue: Int16) {
+    kindred[keyPath: keyPath] = newValue
   }
   
 }
