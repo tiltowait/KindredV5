d5bb7c2f7f2d1c54164a7d33979a4b475c5aeea2
tiltowait <208040+tiltowait@users.noreply.github.com>
2021-07-20 11:48:08 -0700

    Use Popover view

--- Kindred/Tabs/Characters/Individual Character/Derived Traits/RouseResult.swift
+++ Kindred/Tabs/Characters/Individual Character/Derived Traits/RouseResult.swift
@@ -7,11 +7,7 @@
 
 import SwiftUI
 
-struct RouseResult: View, Identifiable {
-  
-  var id = UUID()
-  
-  @State private var scale: CGFloat = 0.8
+struct RouseResult: View {
   
   let title: String
   let imageName: String
@@ -27,34 +23,16 @@ struct RouseResult: View, Identifiable {
   }
   
   var body: some View {
-    VStack(spacing: 20) {
-      Text(title)
-        .font(.title2)
-        .bold()
-      
-      Image(systemName: imageName)
-        .resizable()
-        .scaledToFit()
-        .frame(height: 80)
-    }
-    .foregroundColor(.secondary)
-    .padding()
-    .padding()
-    .background(
-      RoundedRectangle(cornerRadius: 10)
-        .fill(Color.tertiarySystemGroupedBackground)
-        .shadow(radius: 10)
-    )
-    .scaleEffect(scale)
-    .onAppear {
-      withAnimation(
-        .spring(
-          response: 0.3,
-          dampingFraction: 0.5,
-          blendDuration: 0.5
-        )
-      ) {
-        scale = 1
+    Popover {
+      VStack(spacing: 20) {
+        Text(title)
+          .font(.title2)
+          .bold()
+        
+        Image(systemName: imageName)
+          .resizable()
+          .scaledToFit()
+          .frame(height: 80)
       }
     }
   }
